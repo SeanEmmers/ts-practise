365bb7f14a795d61956aa036dd5c09f8c8607d21
Sean Phillips <seanphillips9696@gmail.com>
2021-10-14 15:20:23 +0200

    arrays

---
+++ ts-arrays.ts
@@ -0,0 +1,10 @@
+// forcea array to be number types
+const arr: number[] = []
+
+arr.push(1)
+
+//Tuple, fixed length array with types
+// use ? to make the arguments optional
+type MyList = [number?, string?, boolean?]
+
+const arr1: MyList = []
